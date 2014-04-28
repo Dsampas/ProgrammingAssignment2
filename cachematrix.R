d98ad2064643042796fb600fffac8966adfb5b82
Demetri Sampas <demetrisampas@Demetri-Sampass-MacBook-Pro.local>
2014-04-27 23:22:58 -0400

    bla

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,18 @@
+makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+##set x as y and m as null in environment containing function        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}
+
 cacheinverse <- function(x, ...) {
         m <- x$getinverse()
 ## check to see if inverse has been calculated        
